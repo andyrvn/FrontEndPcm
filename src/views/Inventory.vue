0d466008b8a7fe596ad0b61108f84bb298fdfeba
Andy WIjaya <andy.wijaya@pegipegi.com>
2021-05-01 19:37:39 +0700

    init FE

---
+++ src/views/Inventory.vue
@@ -0,0 +1,22 @@
+<template>
+  <el-container style="height: 100%;">
+    <el-header>
+      <h2>Inventory</h2>
+      <p>Test</p>
+    </el-header>
+    <el-main>
+      <br />
+      <inventory-grid></inventory-grid>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+import InventoryGrid from "../components/InventoryGrid";
+export default {
+  name: "Inventory",
+  components: {
+    "inventory-grid": InventoryGrid
+  }
+};
+</script>
