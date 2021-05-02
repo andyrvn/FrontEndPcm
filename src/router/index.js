1d69ac6ae935cda38abff1cdacb6f5c5e66cb9e7
willoktvn <willoktaviano@gmail.com>
2021-05-02 14:37:03 +0700

    bug fixed

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Login from "../views/Login.vue";
 import Register from "../views/Register.vue";
-//import NotFound from "../views/NotFound";
+import NotFound from "../views/NotFound";
 
 import { storage } from "../utils";
 
@@ -33,18 +33,18 @@ const routes = [
     }
   },
   {
-    /*path: "*",
+    path: "*",
     name: "not-found",
     component: NotFound
-  */
-    path: "*",
+
+    /*path: "*",
     name: "catchAll",
-    component: Home
+    component: Home*/
   }
 ];
 
 const router = new VueRouter({
-  //mode: "history",
+  mode: "hash",
   base: process.env.BASE_URL,
   routes: [
     ...routes,
