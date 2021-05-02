e94505f856122a6950c5db1cb86c2e13d082045b
willoktvn <willoktaviano@gmail.com>
2021-05-03 00:46:17 +0700

    fixed login

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -65,7 +65,7 @@ export default {
       if (userName && password)
         this.login({ userName, password })
           .then(() => {
-            this.$router.push("/dashboard");
+            this.$router.push("/").catch(() => {});
           })
           .catch(err => {
             if (!this.errors.length) this.errors.push(err);
