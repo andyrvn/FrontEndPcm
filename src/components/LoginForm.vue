3888b5ad51ed174af1e92160bd8fa29f0357a30f
willoktvn <willoktaviano@gmail.com>
2021-05-03 08:38:38 +0700

    change routing

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -65,7 +65,7 @@ export default {
       if (userName && password)
         this.login({ userName, password })
           .then(() => {
-            this.$router.push("/");
+            this.$router.push("/").catch(() => {});
           })
           .catch(err => {
             if (!this.errors.length) this.errors.push(err);
