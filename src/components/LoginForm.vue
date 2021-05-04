a3db18ff12cf363f7000d584cf23d77d4f176312
willoktvn <willoktaviano@gmail.com>
2021-05-04 21:55:20 +0700

    bug fixed

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -66,7 +66,7 @@ export default {
         this.login({ userName, password })
           .then(() => {
             //this.$router.push("/").catch(() => {});
-            this.$router.push("/");
+            this.$router.push("../");
           })
           .catch(err => {
             if (!this.errors.length) this.errors.push(err);
